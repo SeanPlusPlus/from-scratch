5299c4e9b203753e33e1e050b029e3a9d470139f
Sean Stephenson <sms@pixar.com>
2017-03-08 15:17:58 -0800

    stub out react

---
+++ 01_react/index.js
@@ -0,0 +1,14 @@
+import log from 'console-emoji'
+
+const emojis = [
+  ':sparkles:',
+  ':ok:',
+  ':dizzy:',
+  ':two_hearts:',
+  ':white_check_mark:',
+  ':beer:',
+]
+
+emojis.forEach((e) => {
+  log(e)
+})
